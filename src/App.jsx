85b0149d2ffbf59c12896736da4d5e557e4877fa
Wenjie24 <122337267+Wenjie24@users.noreply.github.com>
2025-02-05 21:52:08 +0800

    d

--- src/App.jsx
+++ src/App.jsx
@@ -14,24 +14,22 @@ export default function Page() {
   const getNoButtonText = () => {
     const phrases = [
       "No",
-      "Are you sure?",
-      "Really sure?",
-      "Think again!",
-      "Last chance!",
-      "Surely not?",
-      "You might regret this!",
-      "Give it another thought!",
-      "Are you absolutely certain?",
-      "This could be a mistake!",
-      "Have a heart!",
-      "Don't be so cold!",
-      "Change of heart?",
-      "Wouldn't you reconsider?",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
-      "Plsss? :( You're breaking my heart",
+      "u sure :(",
+      "reallyyyy suree :'(",
+      "i give u one kiss :<",
+      "twooooo kiss",
+      "dont liddat mah",
+      "three kiss last chance",
+      "PLEASEEEEEE",
+      "MY WIFEY PLWEASEEEEEEEEE",
+      "U SO HEARTLESS",
+      "I HATE U",
+      "I GOING TO EAT GUCCI",
+      "AND NINI",
+      "U BULLY ME",
+      "U ALWAYS BULLY ME",
+      "DUN LIDDAT MAHHHH",
+      "pls :( ......."
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
@@ -43,25 +41,18 @@ export default function Page() {
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
           <div className="text-4xl md:text-6xl font-bold my-4">
-            Ok Yayyyyy!!!
+            YAYYYY MUAH MUAH MUAHHHHHHHH LOVE YOUUU
           </div>
         </>
       ) : (
         <>
-          <img
-            src={lovesvg}
-            className="fixed animate-pulse top-10 md:left-24 left-6 md:w-40 w-28"
-          />
-          <img
-            src={lovesvg2}
-            className="fixed bottom-16 -z-10 animate-pulse md:right-24 right-10 md:w-40 w-32"
-          />
+         
           <img
             className="h-[230px] rounded-lg shadow-lg"
             src="https://gifdb.com/images/high/cute-Love-bear-roses-ou7zho5oosxnpo6k.gif"
           />
           <h1 className="text-4xl md:text-6xl my-4 text-center">
-            Will you be my Valentine?
+            HIII baby!! Will you be my Valentine?
           </h1>
           <div className="flex flex-wrap justify-center gap-2 items-center">
             <button
@@ -88,14 +79,9 @@ export default function Page() {
 const Footer = () => {
   return (
     <a
-      className="fixed bottom-2 right-2 backdrop-blur-md opacity-80 hover:opacity-95 border p-1 rounded border-rose-300"
-      href="https://github.com/Xeven777/valentine"
-      target="__blank"
+      
     >
-      Made with{" "}
-      <span role="img" aria-label="heart">
-        ❤️
-      </span>
+     
     </a>
   );
 };
